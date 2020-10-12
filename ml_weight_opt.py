d60abcee6b1e0dee03c081eac14ce1f0b56b4404
Jose Matamoros <jmatamoros3@gatech.edu>
2020-10-11 22:48:21 -0400

    Add time info

--- ml_weight_opt.py
+++ ml_weight_opt.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-
+import timeit
 breast_cancer = datasets.load_breast_cancer()
 X = breast_cancer.data
 y = breast_cancer.target
@@ -37,13 +37,44 @@ X_test_std = std_scaler.fit_transform(X_test)
 y_train_std =std_scaler.fit_transform(y_train.reshape(-1, 1))
 y_test_std =std_scaler.fit_transform(y_test.reshape(-1, 1))
 
+#Use 0.01 since that is Adam optimizer default used in p1
 nn_rhc = mlrose.NeuralNetwork(hidden_nodes=[3], activation='relu',
-                                 algorithm='random_hill_climb', max_iters=30,
-                                 bias=True, is_classifier=True, learning_rate=0.0001,
+                                 algorithm='gradient_descent', max_iters=512,
+                                 bias=True, is_classifier=True, learning_rate=0.01,
+                                 early_stopping=False, clip_max=5, max_attempts=100,
+                                 random_state=3)
+s = timeit.default_timer()
+nn_rhc.fit(X_train_std, y_train)
+e = timeit.default_timer()
+print(e-s)
+from sklearn.metrics import accuracy_score
+
+# Predict labels for train set and assess accuracy
+print("Gradient Descent (Benchmark):")
+y_train_pred = nn_rhc.predict(X_train_std)
+
+y_train_accuracy = accuracy_score(y_train, y_train_pred)
+
+print(y_train_accuracy)
+
+# Predict labels for test set and assess accuracy
+y_test_pred = nn_rhc.predict(X_test_std)
+
+y_test_accuracy = accuracy_score(y_test, y_test_pred)
+
+print(y_test_accuracy)
+
+print("="*20)
+
+nn_rhc = mlrose.NeuralNetwork(hidden_nodes=[3], activation='relu',
+                                 algorithm='random_hill_climb', max_iters=512,
+                                 bias=True, restarts=100, is_classifier=True, learning_rate=0.01,
                                  early_stopping=True, clip_max=5, max_attempts=100,
                                  random_state=3)
+s = timeit.default_timer()
 nn_rhc.fit(X_train_std, y_train)
-
+e =  timeit.default_timer()
+print(e-s)
 from sklearn.metrics import accuracy_score
 
 # Predict labels for train set and assess accuracy
@@ -62,9 +93,65 @@ y_test_accuracy = accuracy_score(y_test, y_test_pred)
 print(y_test_accuracy)
 
 print("="*20)
+
+nn_rhc = mlrose.NeuralNetwork(hidden_nodes=[3], activation='relu',
+                                 algorithm='simulated_annealing', max_iters=512,
+                                 bias=True, is_classifier=True, learning_rate=0.01,
+                                 early_stopping=True, clip_max=5, max_attempts=100,
+                                 random_state=3)
+s = timeit.default_timer()
+nn_rhc.fit(X_train_std, y_train)
+e = timeit.default_timer()
+print(e-s)
+from sklearn.metrics import accuracy_score
+
+# Predict labels for train set and assess accuracy
+print("Simulated Annealing:")
+y_train_pred = nn_rhc.predict(X_train_std)
+
+y_train_accuracy = accuracy_score(y_train, y_train_pred)
+
+print(y_train_accuracy)
+
+# Predict labels for test set and assess accuracy
+y_test_pred = nn_rhc.predict(X_test_std)
+
+y_test_accuracy = accuracy_score(y_test, y_test_pred)
+
+print(y_test_accuracy)
+
+print("="*20)
+
+nn_rhc = mlrose.NeuralNetwork(hidden_nodes=[3], activation='relu',
+                                 algorithm='genetic_alg', max_iters=512,
+                                 bias=True, is_classifier=True, learning_rate=0.01,
+                                 early_stopping=True, clip_max=5, max_attempts=100,
+                                 random_state=3)
+s = timeit.default_timer()
+nn_rhc.fit(X_train_std, y_train)
+e = timeit.default_timer()
+print(e-s)
+from sklearn.metrics import accuracy_score
+
+# Predict labels for train set and assess accuracy
+print("Genetic Algorithm:")
+y_train_pred = nn_rhc.predict(X_train_std)
+
+y_train_accuracy = accuracy_score(y_train, y_train_pred)
+
+print(y_train_accuracy)
+
+# Predict labels for test set and assess accuracy
+y_test_pred = nn_rhc.predict(X_test_std)
+
+y_test_accuracy = accuracy_score(y_test, y_test_pred)
+
+print(y_test_accuracy)
+
+print("="*20)
 # nn_sa = mlrose.NeuralNetwork(hidden_nodes=[2], activation='relu',
 #                                  algorithm='random_hill_climb', max_iters=1000,
-#                                  bias=True, is_classifier=True, learning_rate=0.0001,
+#                                  bias=True, is_classifier=True, learning_rate=0.01,
 #                                  early_stopping=True, clip_max=5, max_attempts=100,
 #                                  random_state=3)
 # nn_sa.fit(X_train_std, y_train_std)
@@ -72,7 +159,7 @@ print("="*20)
 #
 # nn_ga = mlrose.NeuralNetwork(hidden_nodes=[2], activation='relu',
 #                                  algorithm='random_hill_climb', max_iters=1000,
-#                                  bias=True, is_classifier=True, learning_rate=0.0001,
+#                                  bias=True, is_classifier=True, learning_rate=0.01,
 #                                  early_stopping=True, clip_max=5, max_attempts=100,
 #                                  random_state=3)
 # nn_ga.fit(X_train_std, y_train_std)
